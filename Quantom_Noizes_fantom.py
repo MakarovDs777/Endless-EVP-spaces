aa2dbbe23b838b9934f5046fbe01e0ec9b774a85
MakarovDs777 <42496353+MakarovDs777@users.noreply.github.com>
2024-05-29 11:06:47 +0300

    Update Quantom_Noizes_fantom.py

--- Quantom_Noizes_fantom.py
+++ Quantom_Noizes_fantom.py
@@ -1,9 +1,11 @@
-from enthought.mayavi import mlab
 import numpy as np
+from mayavi import mlab
 
 def V(x, y, z):
-    return np.cos(10*x)*np.cos(10*y)*np.cos(10*z) +2*(x**2+y**2+z**2)
+    return np.cos(10*x)*np.cos(10*y)*np.cos(10*z) + 2*(x**2 + y**2 + z**2)
 
 X, Y, Z = np.mgrid[-2:2:100j, -2:2:100j, -2:2:100j]
 
-mlab.contour3d(X, Y, Z, V)
+mlab.contour3d(X, Y, Z, V, contours=10)  # Установите количество контуров
+
+mlab.show() 
